b9e9710d178608cb8d21d61a0915f52353fb3c58
Maxim Mishin <maxim.mishin@valamis.com>
2019-03-03 20:22:39 +0300

    Задание №2: вычисление расстояния между двумя точками

---
+++ sandbox/src/main/java/assignment2/AssignmentTwo.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class AssignmentTwo {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(4, 7);
+    Point p2 = new Point(2, 3);
+
+    System.out.println("Расстояния между двумя точками = " + Point.distance(p1,p2));
+  }
+}
+
+
---
+++ sandbox/src/main/java/assignment2/Point.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+public class Point {
+
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+
+    this.x = x;
+    this.y = y;
+
+  }
+
+  public static double square(double a) {
+    return a * a;
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(square(p2.x - p1.x) + square(p2.y - p1.y));
+  }
+}
